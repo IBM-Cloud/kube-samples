61f0d3a194e4f1e83023e79a39e0d2d44b91b991
art <arthur.berger@ibm.com>
2018-02-20 13:45:59 -0500

    Replacing image with yaml

--- daemonset-sample/unrootsquashundo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-grep "^Domain = slnfsv4.com" /etc/idmapd.conf
-if [ "$?" -eq "0" ] ; then
-        sed -i 's/.*Domain =.*/#Domain = slnfsv4.com/g' /etc/idmapd.conf
-        nfsidmap -c
-        service nfs-idmapd stop
-fi
-
