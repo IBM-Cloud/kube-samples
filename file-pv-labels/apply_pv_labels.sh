df69059ba6b31e77144db0511af48e28f4b81b0d
Rachael-Graham <Rachael.Graham@ibm.com>
2018-06-04 20:47:47 -0500

    iks

--- file-pv-labels/apply_pv_labels.sh
+++ file-pv-labels/apply_pv_labels.sh
@@ -2,7 +2,7 @@
 
 #/******************************************************************************
  # * Licensed Materials - Property of IBM
- # * IBM Cloud Container Service, 5737-D42
+ # * IBM Cloud Kubernetes Service, 5737-D42
  # * (C) Copyright IBM Corp. 2018 All Rights Reserved.
  # * US Government Users Restricted Rights - Use, duplication or
  # * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
